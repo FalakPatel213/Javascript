324fa32ee5b67bed12425ff33c652f801dbcbc25
= <=>
2025-01-02 21:10:44 +0530

    Complete lecture-4

---
+++ .devcontainer/01_basic/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// Never use this type of comparison.
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log("2" === 2);        // It strictly checks the type and value both.

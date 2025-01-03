0aee65aa17a9f8471f2368a9e7cf5aced645d619
= <=>
2025-01-03 07:45:35 +0530

    Stack And Heap

--- .devcontainer/01_basic/datatypes-summary.js
+++ .devcontainer/01_basic/datatypes-summary.js
@@ -33,3 +33,32 @@ let myObj = {
 let myFunction = function(){
     console.log("Hello World");
 }
+
+
+
+
+// ***********************************************************************
+
+//  Stack(Primitive), Heap(Non Primitive)
+
+let myGitName = "FalakPatel213"
+
+let anotherName = myGitName;
+
+anotherName = "FalakPatel";
+
+console.log(myGitName); // FalakPatel213
+console.log(anotherName); // FalakPatel
+
+let userOne = {
+    email : "falakpatel213@gmail.com",
+    upi : "falak@icici"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "falak@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);    //Both will be same because both are pointing to same memory location
+

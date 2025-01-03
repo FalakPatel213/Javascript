c4c692f70a052650b4a3114b5a838c30af531c2a
= <=>
2025-01-03 07:28:32 +0530

    Datatype summary

---
+++ .devcontainer/01_basic/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+//  7 types :  String, Number, Boolean, Null, Undifined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLogedIn = false;
+const outsideTemp = null;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+const bigNumber = 5459035594923454434n;
+
+// Reference(Non Primitive)
+
+// Array, Object, Functions
+
+// Array
+
+const heros = ["Shaktimam", "Naagraj", "Doga"];
+
+// Object
+
+let myObj = {
+    name : "Falak",
+    age : 19,
+}
+
+// Function
+
+let myFunction = function(){
+    console.log("Hello World");
+}

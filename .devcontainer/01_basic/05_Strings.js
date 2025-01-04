74f58b4a2fe15624b718b0f7e9e1bdf4d46706c5
= <=>
2025-01-04 07:25:01 +0530

    String add

--- .devcontainer/01_basic/05_Strings.js
+++ .devcontainer/01_basic/05_Strings.js
@@ -1,4 +1,29 @@
-const name = "Falak";
-const repoCount = 50;
+// const name = "Falak";
+// const repoCount = 50;
 
-console.log(`My name is ${name} and I have ${repoCount} repositories on GitHub`);
+// console.log(`My name is ${name} and I have ${repoCount} repositories on GitHub`);
+
+const gameName = new String('FalakPatel');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7,-4);
+// console.log(anotherString);
+
+// const newStringOne = "      Falak       ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://falakpatel.com/falak%20patel";
+console.log(url.replace('%20', '-'));
+

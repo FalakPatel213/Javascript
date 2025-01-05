3f16391b0293c8f7d4885fcf84ccc60568694c64
= <=>
2025-01-05 08:45:30 +0530

    Nums and Math

--- .devcontainer/01_basic/06_NumsAndMaths.js
+++ .devcontainer/01_basic/06_NumsAndMaths.js
@@ -12,5 +12,23 @@
 // console.log(otherNumber.toPrecision(3));
 
 const hundreds = 100000000;
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ******************** Maths ********************
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (min - max + 1)) + min);
 

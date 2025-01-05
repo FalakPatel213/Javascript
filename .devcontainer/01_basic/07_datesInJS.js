7fef7dac6aa2a6e801688b8005fcac58d320e621
= <=>
2025-01-05 09:31:13 +0530

    Dates in JS

---
+++ .devcontainer/01_basic/07_datesInJS.js
@@ -0,0 +1,15 @@
+// Dates
+
+let myDate = new Date();
+console.log(myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toISOString());
+
+let myCreatedDate = new Date(2025, 3, 18);
+
+console.log(myCreatedDate.toDateString());

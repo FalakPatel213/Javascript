884394d7518b1629dbfccce84079f95048a0c4b7
= <=>
2025-01-07 07:18:32 +0530

    Dates update

--- .devcontainer/01_basic/07_datesInJS.js
+++ .devcontainer/01_basic/07_datesInJS.js
@@ -1,7 +1,7 @@
 // Dates
 
-let myDate = new Date();
-console.log(myDate);
+// let myDate = new Date();
+// console.log(myDate);
 
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
@@ -11,5 +11,18 @@ console.log(myDate);
 // console.log(myDate.toISOString());
 
 let myCreatedDate = new Date(2025, 3, 18);
+// console.log(myCreatedDate.toDateString());
 
-console.log(myCreatedDate.toDateString());
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.getDay());
+console.log(myDate.getMonth() + 1);
+
+newDate.toLocalString('default',{
+    Weekday: 'long',
+});

c0a1bc1aae8623b74091dd7c8b4c6d48cf12ce76
= <=>
2025-01-09 08:01:53 +0530

    Object-2

---
+++ .devcontainer/02_basic/04_Objects-2.js
@@ -0,0 +1,20 @@
+const tinderuser = {};
+tinderuser.id = "123abc";
+tinderuser.name = "Sam";
+tinderuser.isLoggedIn = false;
+
+// console.log(tinderuser);
+
+const regularuser = {
+    email: "sone@gmail.com",
+    fullname: {
+        userfullname: {
+            firstName: "Falak",
+            lastname: "Patel",
+        }
+    }
+}
+
+// console.log(regularuser.fullname.userfullname.firstName);
+
+

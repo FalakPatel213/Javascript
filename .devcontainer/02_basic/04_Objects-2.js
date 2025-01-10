b50736e8d60df5d6650790149456166209e96e71
= <=>
2025-01-10 07:58:34 +0530

    Objects-2

--- .devcontainer/02_basic/04_Objects-2.js
+++ .devcontainer/02_basic/04_Objects-2.js
@@ -17,4 +17,26 @@ const regularuser = {
 
 // console.log(regularuser.fullname.userfullname.firstName);
 
+const obj1 = {1: "one", 2: "two"};
+const obj2 = {3: "three", 4: "four"};
 
+// const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = {...obj1, ...obj2};
+// console.log(obj3);
+
+const user = [
+    {
+        id: 1,
+        name: "Sam",
+        age: 25
+    },
+    {
+        id: 2,
+        name: "John",
+        age: 30
+    }
+];
+// console.log(user[0].name);
+
+console.log(tinderuser);
+console.log(tinderuser.hasOwnProperty("id"));

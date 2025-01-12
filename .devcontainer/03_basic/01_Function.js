a80306afd080a819f4d5def72ef8e487dbf4e9ca
= <=>
2025-01-12 08:50:52 +0530

    Function

--- .devcontainer/03_basic/01_Function.js
+++ .devcontainer/03_basic/01_Function.js
@@ -4,18 +4,18 @@ function sayMyName(){
     console.log("L");
     console.log("A");
     console.log("K");
-}
+};
 
 // sayMyName();
 
 // function addTwoNumbers(number1, number2){
 //     console.log(number1 + number2);
-// }
+// };
 
 function addTwoNumbers(number1, number2){
     let result = number1 + number2;
     return result;
-}
+};
 
 // console.log(addTwoNumbers(10, 20));
 
@@ -25,7 +25,32 @@ function loginUserMsg(username = "Falak"){
         return;
     }
     return `${username} just logged in`;
-}
+};
 
-console.log(loginUserMsg());
+// console.log(loginUserMsg());
+
+const user = {
+    username : "Falak",
+    price: 300,
+};
+
+function handleObject(anyObj){
+    console.log(`The username is ${anyObj.username} and price is ${anyObj.price}`);
+};
+
+// handleObject(user);
+
+// handleObject({
+//     username: "Sam",
+//     price: 500
+// });
+
+const myArray = [1, 2, 3, 4, 5];
+
+function returnSecondVal(getArray){
+    return getArray[1];
+};
+
+// console.log(returnSecondVal(myArray));
+// console.log(returnSecondVal([1, 3, 4, 5, 6]));
 

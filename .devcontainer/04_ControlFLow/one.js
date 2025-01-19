0725773a0738d9f6416dc64bfc38d846a40ce414
Falak Patel <falakpatel213@gmail.com>
2025-01-19 09:07:25 +0530

    Control Flow

---
+++ .devcontainer/04_ControlFLow/one.js
@@ -0,0 +1,24 @@
+// IF
+const isUserLoggedIn = true;
+const temp = 40;
+
+// if(2 != 3){
+//     console.log("Executed");
+// }
+
+// <, >, <=, >=, !=, ==, ===(it checks the datatype of the values), !==
+
+// if(temp < 40){
+//     console.log("less then 40");
+// } else{
+//     console.log("greater then 40");
+// }
+// console.log("Executed");
+
+
+const score = 200;
+if(score > 100){
+    const power = "fly";
+    console.log(`User power: ${power}`);    
+}
+

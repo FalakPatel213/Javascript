f6f302effb2788e299da8813854482af36ce67b9
Falak Patel <falakpatel213@gmail.com>
2025-01-19 11:14:49 +0530

    Update control flow

--- .devcontainer/04_ControlFLow/one.js
+++ .devcontainer/04_ControlFLow/one.js
@@ -2,11 +2,14 @@
 const isUserLoggedIn = true;
 const temp = 40;
 
+
 // if(2 != 3){
 //     console.log("Executed");
 // }
 
+
 // <, >, <=, >=, !=, ==, ===(it checks the datatype of the values), !==
+
 
 // if(temp < 40){
 //     console.log("less then 40");
@@ -16,9 +19,25 @@ const temp = 40;
 // console.log("Executed");
 
 
-const score = 200;
-if(score > 100){
-    const power = "fly";
-    console.log(`User power: ${power}`);    
-}
+// const score = 200;
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`User power: ${power}`);    
+// }
 
+
+const balance = 1000;
+
+if(balance < 500){
+    console.log("Less then 500");
+
+}else if(balance < 750){
+    console.log("Less then 750");
+
+}else if(balance < 900){
+    console.log("Less then 900");
+
+}else{
+    console.log("Greater then 1000");
+    
+}

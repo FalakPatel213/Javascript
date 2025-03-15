11a6cabed5a02397218817386ad86e1fe0a8f9f7
Falak Patel <falakpatel213@gmail.com>
2025-03-15 08:08:34 +0530

    Object Properties

--- .devcontainer/10_ObjectOrientedProgramming/mathpi.js
+++ .devcontainer/10_ObjectOrientedProgramming/mathpi.js
@@ -1,3 +1,21 @@
 const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
-console.log(descripter);
+// console.log(descripter);
 
+const falak = {
+    name: "Falak",
+    age: 25,
+    isAvailable: true,
+}
+
+// console.log(Object.getOwnPropertyDescriptor(falak, "name"));
+
+// Object.defineProperty(falak, "name", {
+//     writable: false,
+//     enumerable: false,
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(falak, "name"));
+
+for (let [key, value] of Object.entries(falak)) {
+    console.log(`${key}: ${value}`);
+}
